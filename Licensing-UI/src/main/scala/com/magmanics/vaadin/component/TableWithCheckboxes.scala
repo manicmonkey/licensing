246287a9ff1af97eb295a63b2c21989fdb23a9b9
manicmonkey <j.w.baxter@gmail.com>
2014-09-05 22:27:32 +0100

    Added ability to (de)select all checkboxes at once

--- Licensing-UI/src/main/scala/com/magmanics/vaadin/component/TableWithCheckboxes.scala
+++ Licensing-UI/src/main/scala/com/magmanics/vaadin/component/TableWithCheckboxes.scala
@@ -1,11 +1,15 @@
 package com.magmanics.vaadin.component
 
+import java.util
+
 import com.magmanics.vaadin.ValueChangeListener
 import com.vaadin.data.Property
 import com.vaadin.data.Property.ValueChangeEvent
 import com.vaadin.server.Resource
-import com.vaadin.ui.Table.Align
+import com.vaadin.ui.Table.{Align, HeaderClickEvent, HeaderClickListener}
 import com.vaadin.ui.{CheckBox, Table}
+
+import scala.collection.JavaConverters._
 
 /**
  * @author James Baxter - 05/09/2014.
@@ -16,6 +20,20 @@ trait TableWithCheckboxes extends Table {
 
   addContainerProperty("checkbox", classOf[CheckBox], new CheckBox(), "", null, null)
 
+  addHeaderClickListener(new HeaderClickListener {
+    override def headerClick(p1: HeaderClickEvent) {
+      if ("checkbox".equals(p1.getPropertyId)) {
+        val allChecked = checkboxes.values.forall(c => c.getValue)
+        checkboxes.values.foreach(_.setValue(!allChecked))
+      }
+    }
+  })
+
+  addValueChangeListener(new ValueChangeListener[java.util.Set[AnyRef]](o => {
+    val selectedRows = getValue.asInstanceOf[java.util.Set[AnyRef]]
+    checkboxes.keySet.foreach(i => checkboxes(i).setValue(selectedRows.contains(i)))
+  }))
+
   def containerProperties: Seq[(Any, Class[_], Any, String, Resource, Align)]
 
   containerProperties.foreach(i => addContainerProperty(i._1, i._2, i._3, i._4, i._5, i._6))
@@ -25,11 +43,6 @@ trait TableWithCheckboxes extends Table {
     addItem(checkBox +: item._1, item._2)
     item._2 -> checkBox
   }).toMap
-
-  addListener(new ValueChangeListener[java.util.Set[AnyRef]](o => {
-    val selectedRows = getValue.asInstanceOf[java.util.Set[AnyRef]]
-    checkboxes.keySet.foreach(i => checkboxes(i).setValue(selectedRows.contains(i)))
-  }))
 
   checkboxes.keySet.foreach(i => {
     val checkbox = checkboxes(i)
