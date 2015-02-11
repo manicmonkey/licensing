3837d475e305377bb029507e712589e9fa604f22
James Baxter <j.w.baxter@gmail.com>
2015-02-11 22:43:05 +0000

    Clarify json serialization for jackson

--- Licensing-Model/src/main/scala/com/magmanics/licensing/model/Product.scala
+++ Licensing-Model/src/main/scala/com/magmanics/licensing/model/Product.scala
@@ -24,6 +24,8 @@
 
 package com.magmanics.licensing.model
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+
 import scala.beans.BeanInfo
 
 /**
@@ -31,7 +33,7 @@ import scala.beans.BeanInfo
  * @since 02-Aug-2010
  */
 @BeanInfo
-case class Product(id: Option[Long] = None,
+case class Product(@JsonDeserialize(contentAs = classOf[java.lang.Long]) id: Option[Long] = None,
                    var name: String,
                    var description: String = "",
                    var enabled: Boolean = true,
